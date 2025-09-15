5a0fd558590f010465d09c5f4aa5c62392407633
Samuel Calderon <samuelcalderon1202@hotmail.com>
2025-09-14 23:52:26 -0500

    subida render

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="../ProyectoG3O/static", template_folder="../ProyectoG3O/templates")
+
+@app.route("/")
+def home():
+    return render_template("quienesSomos.html", title="ProyectoG3O")
+
+@app.route("/Links")
+def links():
+    return render_template("LinksRelacionados.html", title = "Links Relacionados")
+
+@app.route("/IngCatastral")
+def ingCat():
+    return render_template("IngCatastralyGeodesia.html", title = "Ingenieria Catastral y Geodesia")
+
+if __name__ == "__main__":
+    app.run(debug=True)
